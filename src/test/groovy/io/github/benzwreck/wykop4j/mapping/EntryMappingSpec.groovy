864d96c4d5d6a13d59fb88d501d785d27ed68a0b
benzwreck <samoa132@o2.pl>
2021-02-10 16:07:50 +0100

    move constants to another class and improve entry mapping test

--- src/test/groovy/io/github/benzwreck/wykop4j/mapping/EntryMappingSpec.groovy
+++ src/test/groovy/io/github/benzwreck/wykop4j/mapping/EntryMappingSpec.groovy
@@ -1,9 +1,8 @@
 package io.github.benzwreck.wykop4j.mapping
 
 import io.github.benzwreck.wykop4j.WykopMappingTestObject
-import io.github.benzwreck.wykop4j.entries.Answer
 import io.github.benzwreck.wykop4j.entries.Entry
-import io.github.benzwreck.wykop4j.entries.Survey
+import io.github.benzwreck.wykop4j.entries.UserVote
 import io.github.benzwreck.wykop4j.profiles.Color
 import spock.lang.Specification
 
@@ -14,40 +13,7 @@ class EntryMappingSpec extends Specification {
 
     def "should map json to Entry class"() {
         when:
-        String json = "{\n" +
-                "   \"data\":{\n" +
-                "      \"id\":55333215,\n" +
-                "      \"date\":\"2021-02-03 21:35:36\",\n" +
-                "      \"body\":\"Ju\\u017c za chwilk\\u0119\",\n" +
-                "      \"author\":{\n" +
-                "         \"login\":\"quatroo96\",\n" +
-                "         \"color\":1,\n" +
-                "         \"avatar\":\"https:\\/\\/www.wykop.pl\\/cdn\\/c3397992\\/avatar_def,q150.png\",\n" +
-                "         \"signup_at\":\"2018-05-21 10:04:29\",\n" +
-                "         \"violation_url\":\"https:\\/\\/a2.wykop.pl\\/naruszenia\\/form\\/ot\\/profile\\/od\\/1502911\\/ud\\/5xOL\\/hs\\/6f8032abfc29cc66d25ce46e12488af58eb88708\\/rn\\/ZhasKJWMCf\\/\"\n" +
-                "      },\n" +
-                "      \"blocked\":false,\n" +
-                "      \"favorite\":false,\n" +
-                "      \"vote_count\":6,\n" +
-                "      \"comments_count\":0,\n" +
-                "      \"status\":\"visible\",\n" +
-                "      \"embed\":{\n" +
-                "         \"type\":\"image\",\n" +
-                "         \"url\":\"https:\\/\\/www.wykop.pl\\/cdn\\/c3201142\\/comment_1612384536orZ67OolQ7sBDR7ndIFbr5.jpg\",\n" +
-                "         \"source\":\"1612384532815.gif\",\n" +
-                "         \"preview\":\"https:\\/\\/www.wykop.pl\\/cdn\\/c3201142\\/comment_1612384536orZ67OolQ7sBDR7ndIFbr5,w400.jpg\",\n" +
-                "         \"plus18\":false,\n" +
-                "         \"size\":\"276KB\",\n" +
-                "         \"animated\":true,\n" +
-                "         \"ratio\":0.982\n" +
-                "      },\n" +
-                "      \"user_vote\":0,\n" +
-                "      \"app\":\"Android\",\n" +
-                "      \"violation_url\":\"https:\\/\\/a2.wykop.pl\\/naruszenia\\/form\\/ot\\/entry\\/od\\/55333215\\/ud\\/5xOL\\/hs\\/69ab7ab81eef21c24ac559438e9ed9125d73d5cb\\/rn\\/RZj76Aozhe\\/\",\n" +
-                "      \"original\":\"#jp2gmd #2137 #jp2 #papiez\\nJu\\u017c za chwilk\\u0119\",\n" +
-                "      \"url\":\"https:\\/\\/www.wykop.pl\\/wpis\\/55333215\\/jp2gmd-2137-jp2-papiez-juz-za-chwilke\\/\"\n" +
-                "   }\n" +
-                "}"
+        String json = SampleEntries.entryWithEmbedJson()
         def entry = mapper.map(json, Entry.class)
         then:
         entry.id() == 55333215
@@ -75,7 +41,7 @@ class EntryMappingSpec extends Specification {
         embed.size() == "276KB"
         embed.animated() == true
         embed.ratio() == 0.982f
-        entry.userVote() == 0
+        entry.userVote() == UserVote.NOT_VOTED
         entry.app().get() == "Android"
         entry.violationUrl().get() == "https://a2.wykop.pl/naruszenia/form/ot/entry/od/55333215/ud/5xOL/hs/69ab7ab81eef21c24ac559438e9ed9125d73d5cb/rn/RZj76Aozhe/"
         entry.original() == "#jp2gmd #2137 #jp2 #papiez\nJuż za chwilkę"
@@ -84,47 +50,7 @@ class EntryMappingSpec extends Specification {
 
     def "should map json to Entry class with survey and canVote"() {
         when:
-        String json = "{\n" +
-                "   \"data\":{\n" +
-                "      \"id\":54760047,\n" +
-                "      \"date\":\"2021-01-08 02:38:08\",\n" +
-                "      \"body\":\"ankieta\",\n" +
-                "      \"author\":{\n" +
-                "         \"login\":\"ZycieJestNobelon\",\n" +
-                "         \"color\":1,\n" +
-                "         \"sex\":\"male\",\n" +
-                "         \"avatar\":\"https:\\/\\/www.wykop.pl\\/cdn\\/c3397992\\/ZycieJestNobelon_2WW7g3RXYw,q150.jpg\",\n" +
-                "         \"signup_at\":\"2017-12-31 19:36:07\",\n" +
-                "         \"background\":\"https:\\/\\/www.wykop.pl\\/cdn\\/c3397992\\/profile_background-ZycieJestNobelon_RnfuZp4Nya,w500.jpg\"\n" +
-                "      },\n" +
-                "      \"blocked\":false,\n" +
-                "      \"favorite\":false,\n" +
-                "      \"vote_count\":0,\n" +
-                "      \"comments_count\":0,\n" +
-                "      \"status\":\"visible\",\n" +
-                "      \"survey\":{\n" +
-                "         \"question\":\"Ankieta\",\n" +
-                "         \"answers\":[\n" +
-                "            {\n" +
-                "               \"id\":1,\n" +
-                "               \"answer\":\"odp1\",\n" +
-                "               \"count\":4,\n" +
-                "               \"percentage\":36.36363636363637\n" +
-                "            },\n" +
-                "            {\n" +
-                "               \"id\":2,\n" +
-                "               \"answer\":\"odp2\",\n" +
-                "               \"count\":7,\n" +
-                "               \"percentage\":63.63636363636363\n" +
-                "            }\n" +
-                "         ]\n" +
-                "      },\n" +
-                "      \"can_comment\":true,\n" +
-                "      \"user_vote\":0,\n" +
-                "      \"original\":\"ankieta\",\n" +
-                "      \"url\":\"https:\\/\\/www.wykop.pl\\/wpis\\/54760047\\/ankieta\\/\"\n" +
-                "   }\n" +
-                "}"
+        String json = SampleEntries.entryWithSurveyAndCanVoteJson()
         def entry = mapper.map(json, Entry.class)
         then:
         entry.canComment() == true
