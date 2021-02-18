dd1a926af48b891c8317db882255c28ea518da4f
benzwreck <samoa132@o2.pl>
2021-02-18 23:50:46 +0100

    simplify SimpleProfile

--- src/main/java/io/github/benzwreck/wykop4j/profiles/SimpleProfile.java
+++ src/main/java/io/github/benzwreck/wykop4j/profiles/SimpleProfile.java
@@ -1,6 +1,5 @@
 package io.github.benzwreck.wykop4j.profiles;
 
-import java.time.LocalDateTime;
 import java.util.Optional;
 
 public class SimpleProfile {
@@ -8,18 +7,13 @@ public class SimpleProfile {
     private final Color color;
     private final Sex sex;
     private final String avatar;
-    private final LocalDateTime signupAt;
-    private final String background;
-    private final String violationUrl;
 
-    public SimpleProfile(String login, Color color, Sex sex, String avatar, LocalDateTime signupAt, String background, String violationUrl) {
+
+    public SimpleProfile(String login, Color color, Sex sex, String avatar) {
         this.login = login;
         this.color = color;
         this.sex = sex;
         this.avatar = avatar;
-        this.signupAt = signupAt;
-        this.background = background;
-        this.violationUrl = violationUrl;
     }
 
     /**
@@ -50,27 +44,6 @@ public class SimpleProfile {
         return avatar;
     }
 
-    /**
-     * @return user's sign up date.
-     */
-    public LocalDateTime signupAt() {
-        return signupAt;
-    }
-
-    /**
-     * @return possible background's url.
-     */
-    public Optional<String> background() {
-        return Optional.ofNullable(background);
-    }
-
-    /**
-     * @return url to violation form.
-     */
-    public String violationUrl() {
-        return violationUrl;
-    }
-
     @Override
     public String toString() {
         return "SimpleProfile{" +
@@ -78,9 +51,6 @@ public class SimpleProfile {
                 ", color=" + color +
                 ", sex=" + sex +
                 ", avatar='" + avatar + '\'' +
-                ", signupAt=" + signupAt +
-                ", background='" + background + '\'' +
-                ", violationUrl='" + violationUrl + '\'' +
                 '}';
     }
 
