a095567b9317652836b44813f38ab07420308b5a
benzwreck <samoa132@o2.pl>
2021-03-15 12:56:14 +0100

    update Color with missing values

--- src/main/java/io/github/benzwreck/wykop4j/profiles/Color.java
+++ src/main/java/io/github/benzwreck/wykop4j/profiles/Color.java
@@ -1,21 +1,69 @@
 package io.github.benzwreck.wykop4j.profiles;
 
 public enum Color {
-    GREEN("#339933"),
-    ORANGE("#FF5917"),
-    CLARET("#BB0000"),
-    ADMIN("#000000"),
-    BANNED("#999999"),
-    DELETED("#999999"),
-    CLIENT("#3F6FA0"),
-    UNDEFINED("undefined");
-    private final String hex;
+    GREEN("#339933", RGB.GREEN),
+    ORANGE("#FF5917", RGB.ORANGE),
+    CLARET("#BB0000", RGB.CLARET),
+    ADMIN("#000000", RGB.BLACK),
+    BANNED("#999999", RGB.GRAY),
+    DELETED("#999999", RGB.GRAY),
+    CLIENT("#3F6FA0", RGB.BLUE),
+    PLAY("#593787", RGB.PURPLE),
+    GOLD("#bf9b30", RGB.GOLD),
+    UNDEFINED("undefined", RGB.UNDEFINED);
 
-    Color(String hex) {
+    private final String hex;
+    private final RGB rgb;
+
+    Color(String hex, RGB rgb) {
         this.hex = hex;
+        this.rgb = rgb;
     }
 
     public String hex() {
         return hex;
     }
+
+    public enum RGB {
+        GREEN(51, 153, 51),
+        ORANGE(255, 89, 23),
+        CLARET(187, 0, 0),
+        BLACK(0, 0, 0),
+        PURPLE(72,34,124),
+        GOLD(191,155, 48),
+        GRAY(153, 153,153),
+        BLUE(63, 111, 160),
+        UNDEFINED(255,255,255);
+
+        private final int red;
+        private final int green;
+        private final int blue;
+
+        RGB(int red, int green, int blue) {
+            this.red = red;
+            this.green = green;
+            this.blue = blue;
+        }
+
+        public int red() {
+            return red;
+        }
+
+        public int green() {
+            return green;
+        }
+
+        public int blue() {
+            return blue;
+        }
+
+        @Override
+        public String toString() {
+            return "RGB{" +
+                    "red=" + red +
+                    ", green=" + green +
+                    ", blue=" + blue +
+                    '}';
+        }
+        }
 }
