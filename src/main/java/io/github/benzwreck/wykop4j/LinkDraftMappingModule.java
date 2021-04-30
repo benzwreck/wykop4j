c0f3e5bf85c359d99f32b00d805fb69ee6d3ea26
benzwreck <samoa132@o2.pl>
2021-04-30 14:04:58 +0200

    update link's draft class with missing fields

--- src/main/java/io/github/benzwreck/wykop4j/LinkDraftMappingModule.java
+++ src/main/java/io/github/benzwreck/wykop4j/LinkDraftMappingModule.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.module.SimpleModule;
-import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import io.github.benzwreck.wykop4j.links.Link;
 import io.github.benzwreck.wykop4j.links.LinkDraft;
@@ -26,18 +25,23 @@ class LinkDraftMappingModule extends SimpleModule {
         @Override
         public LinkDraft deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
             ObjectNode nodes = p.getCodec().readTree(p);
-            JsonNode data = nodes.get("data");
+            JsonNode data = nodes.hasNonNull("data") ? nodes.get("data") : nodes;
             String key = data.get("key").asText();
+            String title = data.hasNonNull("title") ? data.get("title").asText() : null;
+            String description = data.hasNonNull("description") ? data.get("description").asText() : null;
+            String tags = data.hasNonNull("tags") ? data.get("tags").asText() : null;
             String sourceUrl = data.get("source_url").asText();
             List<Link> duplicates = new ArrayList<>();
-            for (Iterator<JsonNode> it = nodes.get("duplicates").elements(); it.hasNext(); ) {
-                JsonNode node = it.next();
-                JsonParser parser = node.traverse(p.getCodec());
-                parser.nextToken();
-                Link link = ctxt.readValue(parser, Link.class);
-                duplicates.add(link);
+            if (nodes.hasNonNull("duplicates")) {
+                for (Iterator<JsonNode> it = nodes.get("duplicates").elements(); it.hasNext(); ) {
+                    JsonNode node = it.next();
+                    JsonParser parser = node.traverse(p.getCodec());
+                    parser.nextToken();
+                    Link link = ctxt.readValue(parser, Link.class);
+                    duplicates.add(link);
+                }
             }
-            return new LinkDraft(key, sourceUrl, duplicates);
+            return new LinkDraft(key, sourceUrl, title, description, tags, duplicates);
         }
     }
 }
--- src/main/java/io/github/benzwreck/wykop4j/links/LinkDraft.java
+++ src/main/java/io/github/benzwreck/wykop4j/links/LinkDraft.java
@@ -2,15 +2,22 @@ package io.github.benzwreck.wykop4j.links;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 public class LinkDraft {
     private final String key;
     private final String sourceUrl;
+    private final String title;
+    private final String description;
+    private final String tags;
     private final List<Link> duplicates;
 
-    public LinkDraft(String key, String sourceUrl, List<Link> duplicates) {
+    public LinkDraft(String key, String sourceUrl, String title, String description, String tags, List<Link> duplicates) {
         this.key = key;
         this.sourceUrl = sourceUrl;
+        this.title = title;
+        this.description = description;
+        this.tags = tags;
         this.duplicates = duplicates;
     }
 
@@ -26,11 +33,26 @@ public class LinkDraft {
         return Collections.unmodifiableList(duplicates);
     }
 
+    public Optional<String> title() {
+        return Optional.of(title);
+    }
+
+    public Optional<String> description() {
+        return Optional.of(description);
+    }
+
+    public Optional<String> tags() {
+        return Optional.of(tags);
+    }
+
     @Override
     public String toString() {
         return "LinkDraft{" +
                 "key='" + key + '\'' +
                 ", sourceUrl='" + sourceUrl + '\'' +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", tags='" + tags + '\'' +
                 ", duplicates=" + duplicates +
                 '}';
     }
