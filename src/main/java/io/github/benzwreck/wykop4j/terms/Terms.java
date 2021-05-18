1cf7b8eb629dc2952192b2b9c629b16c06af7c2d
benzwreck <samoa132@o2.pl>
2021-05-18 13:48:43 +0200

    update terms javadocs

--- src/main/java/io/github/benzwreck/wykop4j/terms/Terms.java
+++ src/main/java/io/github/benzwreck/wykop4j/terms/Terms.java
@@ -1,5 +1,8 @@
 package io.github.benzwreck.wykop4j.terms;
 
+/**
+ * This class contains Wykop Terms.
+ */
 public class Terms {
     private final String html;
     private final String text;
@@ -9,10 +12,16 @@ public class Terms {
         this.text = text;
     }
 
+    /**
+     * Gets the html version of Wykop Terms with html tags.
+     */
     public String html() {
         return html;
     }
 
+    /**
+     * Gets plain text version of Wykop Terms.
+     */
     public String text() {
         return text;
     }
