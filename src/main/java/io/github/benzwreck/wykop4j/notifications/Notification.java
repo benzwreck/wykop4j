edc84c36817b5edd7cf8d689de07953e5efbb976
benzwreck <samoa132@o2.pl>
2021-05-18 13:46:45 +0200

    update notification javadocs

--- src/main/java/io/github/benzwreck/wykop4j/notifications/Notification.java
+++ src/main/java/io/github/benzwreck/wykop4j/notifications/Notification.java
@@ -4,6 +4,9 @@ import io.github.benzwreck.wykop4j.profiles.SimpleProfile;
 
 import java.time.LocalDateTime;
 
+/**
+ * This class represents a single notification e.g. new private message, replies, warnings.
+ */
 public class Notification {
     private final Long id;
     private final SimpleProfile author;
@@ -27,38 +30,65 @@ public class Notification {
         this.isNew = isNew;
     }
 
+    /**
+     * Gets notification's id.
+     */
     public Long id() {
         return id;
     }
 
+    /**
+     * Gets notification's author {@link SimpleProfile}.
+     */
     public SimpleProfile author() {
         return author;
     }
 
+    /**
+     * Gets notification's date and time.
+     */
     public LocalDateTime date() {
         return date;
     }
 
+    /**
+     * Gets notification's body.
+     */
     public String body() {
         return body;
     }
 
+    /**
+     * Gets notification's type.
+     */
     public String type() {
         return type;
     }
 
+    /**
+     * Gets the id of item e.g. entry, link, comment.
+     */
     public String itemId() {
         return itemId;
     }
 
+    /**
+     * Gets the if of subitem e.g. if you replied to a link's comment then subitem will be link's id.
+     */
     public String subitemId() {
         return subitemId;
     }
 
+    /**
+     * Gets notification's url.
+     */
     public String url() {
         return url;
     }
 
+    /**
+     * Returns if the notification is new or already read.
+     */
     public Boolean isNew() {
         return isNew;
     }
